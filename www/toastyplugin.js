24a7c1d80ac149e07762aaee116d91380b5cd6f8
ammarshatta <43485980+ammarshatta@users.noreply.github.com>
2021-06-22 18:31:22 +0200

    Update toastyplugin.js

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -1,13 +1,13 @@
 // Empty constructor
-function ToastyPlugin() {}
+function AmmarPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+AmmarPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
   options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
+  cordova.exec(successCallback, errorCallback, 'AmmarPlugin', 'show', [options]);
 }
 
 // Installation constructor that binds ToastyPlugin to window
@@ -15,7 +15,7 @@ ToastyPlugin.install = function() {
   if (!window.plugins) {
     window.plugins = {};
   }
-  window.plugins.toastyPlugin = new ToastyPlugin();
+  window.plugins.AmmarPlugin = new AmmarPlugin();
   return window.plugins.toastyPlugin;
 };
-cordova.addConstructor(ToastyPlugin.install);
+cordova.addConstructor(AmmarPlugin.install);
